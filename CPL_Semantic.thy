777a5e4338877a33103330f7d6a0751f63285887
brodriguez059 <brodriguez.ecole@gmail.com>
2024-04-01 05:33:46 +0200

    Separate Syntax and Semantic

---
+++ CPL_Semantic.thy
@@ -0,0 +1,123 @@
+theory "CPL_Semantic"
+  imports Main "CPL_Syntax"
+begin
+
+(* ==================== Semantics ==================== *)
+
+(* ======================== Type Definitions ======================== *)
+
+type_synonym Signature =  "Relation \<rightharpoonup> nat" (* Represented by \<S> *)
+type_synonym 'a Interpretation = "Relation \<rightharpoonup> 'a list set" (* Represented by \<I> *)
+datatype 'a Structure =  Structure (Sig: "Signature") (Univ: "'a set")  (Interp: "'a Interpretation") (* Represented by \<B>, and internally by \<S> universe \<I> *)
+
+type_synonym 'a Valuation = "Variable \<rightharpoonup> 'a" (* Represented by f *)
+datatype 'a Judgement =  Judgement (Index: "nat") (Vars:  "Variable set") (Funcs:  "'a Valuation set") (* Represented by \<J>, and internally by index \<V> \<F> *)
+
+(* ======================== Auxiliary Functions ======================== *)
+
+fun formulaToList :: "Formula \<Rightarrow> Formula list" where
+"formulaToList (Atom r var_list) = [Atom r var_list]" |
+"formulaToList (And \<phi>\<^sub>1 \<phi>\<^sub>2) = [And \<phi>\<^sub>1 \<phi>\<^sub>2, \<phi>\<^sub>1, \<phi>\<^sub>2] @ (tl (formulaToList \<phi>\<^sub>1)) @ (tl (formulaToList \<phi>\<^sub>2))" |
+"formulaToList (Forall x \<phi>) = [Forall x \<phi>] @ (formulaToList \<phi>)" |
+"formulaToList (Exists x \<phi>) = [Exists x \<phi>] @ (formulaToList \<phi>)"
+
+fun setOfIndex :: "Formula list \<Rightarrow> nat set" where
+"setOfIndex formula_list = { 1 .. (length formula_list) }"
+
+fun FoI :: "nat \<Rightarrow> Formula list \<Rightarrow> Formula" where
+"FoI formula_index formula_list = formula_list ! (formula_index - 1)"
+
+(* ======================== Well-Formedness Functions ======================== *)
+
+fun wfFormula :: "Formula \<Rightarrow> Signature \<Rightarrow> bool" where
+"wfFormula (Atom r var_list) \<S> = (
+    case (\<S> r) of
+    None \<Rightarrow> False |
+    (Some n) \<Rightarrow> ((length var_list) = n)
+)" |
+"wfFormula (And \<phi>\<^sub>1 \<phi>\<^sub>2) \<S> = ((wfFormula \<phi>\<^sub>1 \<S>) \<and> (wfFormula \<phi>\<^sub>2 \<S>))" |
+"wfFormula (Forall x \<phi>) \<S> = (wfFormula \<phi> \<S>)" |
+"wfFormula (Exists x \<phi>) \<S> = (wfFormula \<phi> \<S>)"
+
+(* Note: Añadimos (set l \<subseteq> universe) para que se confirme que los datos pertenecen al dominio *)
+fun wfStructure :: "'a Structure \<Rightarrow> bool" where
+"wfStructure (Structure \<S> universe \<I>) = (
+  ( universe \<noteq> {} )  \<and>
+  ( \<forall>r. r \<in> (dom \<S>) \<longrightarrow> (
+      case (\<I> r) of
+      None \<Rightarrow> False |
+      (Some set_of_lists) \<Rightarrow> (case (\<S> r) of
+         None \<Rightarrow> False |
+         (Some n) \<Rightarrow> (\<forall>l \<in> set_of_lists. ((length l) = n) \<and> ((set l) \<subseteq> universe))
+      )
+    )
+  )
+)"
+
+fun wfCPLInstance :: "Formula \<Rightarrow> 'a Structure \<Rightarrow> bool" where
+"wfCPLInstance \<phi> \<B> = (
+  (sentence \<phi>) \<and>  
+  (wfStructure \<B>) \<and>
+  (wfFormula \<phi> (Sig \<B>))
+)"
+
+fun wfJudgement :: "'a Judgement \<Rightarrow> Formula \<Rightarrow> 'a Structure \<Rightarrow> bool" where
+"wfJudgement \<J> \<phi> \<B> = (let
+    formula_list = formulaToList \<phi>;
+    judgement_index = (Index \<J>);
+    judgement_free_vars = (Vars \<J>);
+    judgement_valuations = (Funcs \<J>);
+    structure_domain = (Univ \<B>)
+  in (
+    ( wfCPLInstance \<phi> \<B> ) \<and>
+    ( judgement_index \<in> (setOfIndex formula_list) ) \<and>
+    ( judgement_free_vars \<subseteq> (freeVar (FoI judgement_index formula_list)) ) \<and>
+    ( \<forall>f \<in> judgement_valuations. ((dom f) = judgement_free_vars) )  \<and>
+    ( \<forall>f \<in> judgement_valuations. ((ran f) \<subseteq> structure_domain) )
+  )
+)"
+
+(* ==================== Tests ==================== *)
+
+abbreviation "myUniverse::(BEnum set) \<equiv> {A,B,C}"
+abbreviation "mySignature::(Signature) \<equiv> [CHR ''E'' \<mapsto> 2]"
+abbreviation "myFormula \<equiv> (
+  Exists (CHR ''x'') (
+    Forall (CHR ''y'') (
+      And (
+        (Atom (CHR ''E'') [CHR ''x'', CHR ''y'']))
+        (Exists (CHR ''x'') (
+          Atom (CHR ''E'') [CHR ''x'',CHR ''y'']
+        )
+      )
+    )
+  )
+)"
+
+lemma "wfFormula myFormula mySignature = True"
+  by auto
+
+
+
+abbreviation "myInterpretation::(BEnum Interpretation) \<equiv> [CHR ''E'' \<mapsto> {[A,A],[A,C],[B,A]}]"
+abbreviation "myStructure::(BEnum Structure) \<equiv> (Structure mySignature myUniverse myInterpretation)"
+
+lemma "wfStructure myStructure = True"
+  by auto
+
+
+
+abbreviation "myValuationSet::(BEnum Valuation set) \<equiv> {
+  [CHR ''x'' \<mapsto> A, CHR ''y'' \<mapsto> A],
+  [CHR ''x'' \<mapsto> A, CHR ''y'' \<mapsto> C],
+  [CHR ''x'' \<mapsto> B, CHR ''y'' \<mapsto> A]
+}"
+abbreviation "myFreeVariableSet::(Variable set) \<equiv> {CHR ''x'', CHR ''y''}"
+abbreviation "myJudgement::(BEnum Judgement) \<equiv> (Judgement 6 myFreeVariableSet myValuationSet)"
+
+lemma "wfJudgement myJudgement myFormula myStructure = True"
+  apply(simp_all)
+  apply(simp add: Let_def)
+  by auto
+
+end
---
+++ CPL_Syntax.thy
@@ -0,0 +1,167 @@
+theory "CPL_Syntax"
+  imports Main
+begin
+
+(* ==================== Syntax ==================== *)
+
+(* ======================== Type Definitions ======================== *)
+
+(* Note: Explain the caveats of using string. We will have to keep using char for now *)
+type_synonym Variable =  "char"
+type_synonym Relation =  "char"
+datatype Formula =
+Atom (atom_rel: "Relation") (atom_vars: "Variable list") |
+And (and_f1: "Formula") (and_f2: "Formula") |
+Forall (forall_x: "Variable") (forall_f: "Formula") |
+Exists (exists_x: "Variable") (exists_f: "Formula")
+
+(* ======================== Auxiliary Functions ======================== *)
+
+fun freeVar :: "Formula \<Rightarrow> Variable set" where
+"freeVar (Atom r var_list) = set var_list" |
+"freeVar (And \<phi>\<^sub>0 \<phi>\<^sub>1) = (freeVar \<phi>\<^sub>0) \<union> (freeVar \<phi>\<^sub>1)" |
+"freeVar (Forall x \<phi>) = (freeVar \<phi>) - {x}" |
+"freeVar (Exists x \<phi>) = (freeVar \<phi>) - {x}"
+
+fun sentence :: "Formula \<Rightarrow> bool" where
+"sentence \<phi> = ((freeVar \<phi>) = {})"
+
+(* ======================== Extra Functions ======================== *)
+
+fun isFormulaAtom :: "Formula \<Rightarrow> bool" where
+"isFormulaAtom (Atom r var_list) = True" |
+"isFormulaAtom _ = False"
+
+fun isFormulaAnd :: "Formula \<Rightarrow> bool" where
+"isFormulaAnd (And \<phi>\<^sub>1 \<phi>\<^sub>2) = True" |
+"isFormulaAnd _ = False"
+
+fun isFormulaForAll :: "Formula \<Rightarrow> bool" where
+"isFormulaForAll (Forall x \<phi>) = True" |
+"isFormulaForAll _ = False"
+
+fun isFormulaExists :: "Formula \<Rightarrow> bool" where
+"isFormulaExists (Exists x \<phi>) = True" |
+"isFormulaExists _ = False"
+
+(* ======================== Tests ======================== *)
+
+datatype BEnum = A | B | C (* Finite datatype *)
+
+lemma BEnum_induct: "\<lbrakk>x \<noteq> A; x \<noteq> B\<rbrakk> \<Longrightarrow> x = C"
+  apply(cases x)
+  by auto
+
+lemma UNIV_BEnum: "UNIV = {A, B, C}"
+  by (auto simp add: BEnum_induct)
+
+(* Note: We must indicate that this type is an enum to use ran and dom *)
+instantiation BEnum :: enum
+begin
+  definition "Enum.enum = [A,B,C]"
+  definition "Enum.enum_all P \<longleftrightarrow> (Ball {A,B,C} P)"
+  definition "Enum.enum_ex P \<longleftrightarrow> (Bex {A,B,C} P)"
+  
+  instance proof
+  qed (auto simp add: enum_BEnum_def enum_all_BEnum_def enum_ex_BEnum_def UNIV_BEnum)
+end
+
+
+
+abbreviation "atomFormula::Formula \<equiv> (Atom (CHR ''E'') [CHR ''x'', CHR ''y''])"
+
+lemma "freeVar atomFormula = {CHR ''x'', CHR ''y''}"
+  by auto
+
+lemma "sentence atomFormula = False"
+  by auto
+
+lemma "isFormulaAtom atomFormula = True"
+  by auto
+
+lemma "isFormulaForAll atomFormula = False"
+  by auto
+
+lemma "isFormulaExists atomFormula = False"
+  by auto
+
+lemma "isFormulaAnd atomFormula = False"
+  by auto
+
+
+
+abbreviation "forAllFormula::Formula \<equiv> (Forall (CHR ''x'') atomFormula)"
+
+lemma "freeVar forAllFormula = {CHR ''y''}"
+  by auto
+
+lemma "sentence forAllFormula = False"
+  by auto
+
+lemma "isFormulaAtom forAllFormula = False"
+  by auto
+
+lemma "isFormulaForAll forAllFormula = True"
+  by auto
+
+lemma "isFormulaExists forAllFormula = False"
+  by auto
+
+lemma "isFormulaAnd forAllFormula = False"
+  by auto
+
+
+
+abbreviation "existsFormula::Formula \<equiv> (Exists (CHR ''y'') atomFormula)"
+
+lemma "freeVar existsFormula = {CHR ''x''}"
+  by auto
+
+lemma "sentence existsFormula = False"
+  by auto
+
+lemma "isFormulaAtom existsFormula = False"
+  by auto
+
+lemma "isFormulaForAll existsFormula = False"
+  by auto
+
+lemma "isFormulaExists existsFormula = True"
+  by auto
+
+lemma "isFormulaAnd existsFormula = False"
+  by auto
+
+
+
+abbreviation "andFormula::Formula \<equiv> (And forAllFormula existsFormula)"
+
+lemma "freeVar andFormula = {CHR ''x'', CHR ''y''}"
+  by auto
+
+lemma "sentence andFormula = False"
+  by auto
+
+lemma "isFormulaAtom andFormula = False"
+  by auto
+
+lemma "isFormulaForAll andFormula = False"
+  by auto
+
+lemma "isFormulaExists andFormula = False"
+  by auto
+
+lemma "isFormulaAnd andFormula = True"
+  by auto
+
+
+
+abbreviation "sentenceFormula::Formula \<equiv> (Forall (CHR ''x'') existsFormula)"
+
+lemma "freeVar sentenceFormula = {}"
+  by auto
+
+lemma "sentence sentenceFormula = True"
+  by auto
+
+end
